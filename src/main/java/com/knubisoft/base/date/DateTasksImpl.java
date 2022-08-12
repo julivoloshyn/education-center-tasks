3c0bf5ec14068d26e70b575a70c46a3917856ad7
julivoloshyn <yuliavoloshina02@gmail.com>
2022-08-12 10:56:53 +0300

    Date tasks are done

--- src/main/java/com/knubisoft/base/date/DateTasksImpl.java
+++ src/main/java/com/knubisoft/base/date/DateTasksImpl.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -18,9 +19,9 @@ public class DateTasksImpl implements DateTasks {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
         Calendar calendar = Calendar.getInstance();
-        try{
+        try {
             calendar.setTime(simpleDateFormat.parse(date));
-        }catch(ParseException e){
+        } catch (ParseException e) {
             e.printStackTrace();
         }
 
@@ -41,17 +42,41 @@ public class DateTasksImpl implements DateTasks {
 
     @Override
     public String findBiggestDate(String date1, String date2, String date3) {
-        return null;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDate date11 = LocalDate.parse(date1, formatter);
+        LocalDate date22 = LocalDate.parse(date2, formatter);
+        LocalDate date33 = LocalDate.parse(date3, formatter);
+
+        if (date11.compareTo(date22) > 0) {
+            if (date11.compareTo(date33) > 0) {
+                return date1;
+            } else {
+                return date3;
+            }
+        } else if (date22.compareTo(date33) > 0) {
+            return date2;
+        } else {
+            return date3;
+        }
     }
 
     @Override
     public String getLastDayOfTheMonth(String date) {
-        return null;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate newDate = LocalDate.parse(date, formatter);
+
+        return newDate.plusDays(newDate.lengthOfMonth() - newDate.getDayOfMonth()).toString();
     }
 
     @Override
     public String sumTimes(String time1, String time2) {
-        return null;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        return LocalTime.parse(time1).plusSeconds(LocalTime.parse(time2)
+                .toSecondOfDay()).format(formatter);
     }
 
     @Override
@@ -60,9 +85,9 @@ public class DateTasksImpl implements DateTasks {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
         Calendar calendar = Calendar.getInstance();
-        try{
+        try {
             calendar.setTime(simpleDateFormat.parse(date));
-        }catch(ParseException e){
+        } catch (ParseException e) {
             e.printStackTrace();
         }
 
@@ -88,7 +113,7 @@ public class DateTasksImpl implements DateTasks {
         String previousFriday = String.valueOf(today.with(TemporalAdjusters.previous(DayOfWeek.FRIDAY)));
         String nextFriday = String.valueOf(today.with(TemporalAdjusters.next(DayOfWeek.FRIDAY)));
 
-        return new String[] {previousFriday, nextFriday};
+        return new String[]{previousFriday, nextFriday};
     }
 
     @Override
