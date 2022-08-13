f36dc03f64ed5a8cdd77722ebf2b9e1e5fe02f2e
julivoloshyn <yuliavoloshina02@gmail.com>
2022-08-13 12:43:24 +0300

    Boolean tasks are done

--- src/main/java/com/knubisoft/base/bool/BoolTasksImpl.java
+++ src/main/java/com/knubisoft/base/bool/BoolTasksImpl.java
@@ -1,5 +1,7 @@
 package com.knubisoft.base.bool;
 
+import java.util.Locale;
+
 public class BoolTasksImpl implements BoolTasks {
 
     @Override
@@ -28,36 +30,80 @@ public class BoolTasksImpl implements BoolTasks {
 
     @Override
     public Boolean isFalseUnbox(boolean value) {
-        return null;
+
+        Boolean b = value;
+
+        return b == false ? b : true;
     }
 
     @Override
     public boolean andFunction(int digit, String string) {
+
+        int num;
+
+        try {
+            num = Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        if(digit == num && string != null){
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public boolean orFunction(int digit, String string) {
-        return false;
+
+        int strNum;
+
+        try {
+            strNum = Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        if(digit != strNum || string == null){
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public boolean andComplexFunction(int generatedFirstDigit, double generatedSecondDigit, int range) {
-        return false;
+
+        double result = Math.round(generatedSecondDigit) & range;
+
+        return result == generatedFirstDigit;
     }
 
     @Override
     public boolean orComplexFunction(int generatedFirstDigit, double generatedSecondDigit, int generatedThirdDigit, int range) {
-        return false;
+
+        double result1 = Math.round(generatedSecondDigit) | generatedFirstDigit;
+        double result2 = Math.round(generatedThirdDigit) | generatedFirstDigit;
+
+        return  ((result1) < (range) && (range) < result2) || (result2 < range && range < result1);
     }
 
     @Override
     public boolean isSameSizeArray(Object[] firstArray, Object... secondArray) {
-        return false;
+
+        if(firstArray == null || secondArray == null){
+            return false;
+        } else return firstArray.length == secondArray.length;
     }
 
     @Override
     public boolean isSameCharactersCount(String username, String name, int maxLength) {
-        return false;
+
+        if (username == null || name == null || maxLength == 0) {
+            return false;
+        } else {
+            return username.toCharArray().length == name.toCharArray().length;
+        }
     }
 }
