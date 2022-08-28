8a74ff3416c85165ffed9b190f15e3e280b347b2
julivoloshyn <yuliavoloshina02@gmail.com>
2022-08-28 21:30:39 +0300

    lucky ticket is done

--- src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
@@ -1,9 +1,38 @@
 package com.knubisoft.tasks.algorithm.luckyticket;
 
+
 public class LuckyTicketImpl implements LuckyTicket {
 
     @Override
     public boolean checkIsLuckyTicket(String ticket) {
-        return false;
+
+        if(ticket == null){
+            return false;
+        }
+
+        boolean isLucky = false;
+        boolean onlyDigits = ticket.matches("[0-9]+");
+        int length = ticket.length();
+
+        if(length < 6 || length > 8 || !onlyDigits){
+            isLucky = false;
+        } else {
+
+            if(isPalindrome(ticket)){
+                isLucky = true;
+            }
+        }
+        return isLucky;
+    }
+
+    private boolean isPalindrome(String ticket) {
+        StringBuilder reverse = new StringBuilder();
+        char[] plain = ticket.toCharArray();
+
+        for (int i = plain.length - 1; i >= 0; i--) {
+            reverse.append(plain[i]);
+        }
+
+        return (reverse.toString()).equals(ticket);
     }
 }
