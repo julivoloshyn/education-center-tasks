77f568d908638225fe8eb31411a519610e7e55e5
julivoloshyn <yuliavoloshina02@gmail.com>
2022-09-23 18:24:14 +0300

    Json tasks are done

--- src/main/java/com/knubisoft/tasks/algorithm/json/JsonTasksImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/json/JsonTasksImpl.java
@@ -1,32 +1,80 @@
 package com.knubisoft.tasks.algorithm.json;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.knubisoft.tasks.algorithm.ModelRoot;
 import lombok.SneakyThrows;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class JsonTasksImpl implements JsonTasks {
 
     @Override
     @SneakyThrows
     public ModelRoot parseJson(String json) {
-        return null;
+
+        if (json.isEmpty()) {
+            throw new NullPointerException();
+        }
+
+        ObjectMapper jsonMapper = new ObjectMapper();
+        jsonMapper.findAndRegisterModules();
+
+        return jsonMapper.readValue(json, ModelRoot.class);
     }
 
     @Override
     public List<Integer> getAllIds(String json) {
-        return null;
+
+        ModelRoot modelRoot = parseJson(json);
+
+        List<Integer> idsList = new ArrayList<>();
+
+        int i = 0;
+        for (ModelRoot.Item item : modelRoot.getItems()) {
+            idsList.add(i, item.id);
+            i++;
+        }
+        return idsList;
     }
 
     @Override
     public List<String> getNameWithIdMoreThan1(String json) {
-        return null;
+
+        ModelRoot modelRoot = parseJson(json);
+
+        List<String> namesList = new ArrayList<>();
+
+        for (ModelRoot.Item item : modelRoot.getItems()) {
+            if (item.id > 1) {
+                namesList.add(item.name);
+            }
+        }
+
+        return namesList;
     }
 
+    @SneakyThrows
     @Override
-    public List<Map<String,String>> getAllItems(String json) {
-        return null;
+    public List<Map<String, String>> getAllItems(String json) {
+
+        if (json.isEmpty()) {
+            throw new NullPointerException();
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        ModelRoot modelRoot = mapper.readValue(json, ModelRoot.class);
+
+        List<ModelRoot.Item> items = modelRoot.getItems();
+        Map<String, String> map = new HashMap<>();
+
+        return items.stream().map(item -> getMap(item, map)).collect(Collectors.toList());
+    }
+
+    private Map<String, String> getMap(ModelRoot.Item item, Map<String, String> map) {
+        map.put(item.getName(), item.toString());
+
+        return map;
     }
 
 }
--- src/test/java/com/knubisoft/tasks/algorithm/json/JsonTasksImplTest.java
+++ src/test/java/com/knubisoft/tasks/algorithm/json/JsonTasksImplTest.java
@@ -1,10 +1,12 @@
 package com.knubisoft.tasks.algorithm.json;
 
 import com.knubisoft.tasks.algorithm.ModelRoot;
+import lombok.SneakyThrows;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class JsonTasksImplTest {
 
@@ -26,17 +29,21 @@ class JsonTasksImplTest {
     @Test
     void parseJson() {
         ModelRoot result = object.parseJson(content);
+        InputStream wrongPath = ModelRoot.class.getClassLoader().getResourceAsStream("js.s");
 
         assertEquals(3, result.items.size());
-        /*
-            TODO add extra checks
-         */
+
+        assertEquals(result.getItems().get(0), result.getItems().get(0));
+
+        assertEquals("Cake3:3", result.getItems().get(2).name + ":" + result.getItems().get(2).id);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> result.items.get(result.getItems().size() + 1));
+        assertThrows(NullPointerException.class, () -> object.parseJson(wrongPath.toString()));
     }
 
     @Test
     void getAllIds() {
         List<Integer> result = object.getAllIds(content);
-
         assertEquals(Arrays.asList(1,2,3), result);
     }
 
@@ -46,13 +53,16 @@ class JsonTasksImplTest {
         assertEquals(Arrays.asList("Cake2","Cake3"), result);
     }
 
+    @SneakyThrows
     @Test
     void getAllItems() {
         List<Map<String, String>> result = object.getAllItems(content);
+        String wrongFile = IOUtils.toString(Objects.requireNonNull(
+                getClass().getClassLoader().getResourceAsStream("test.json")), StandardCharsets.UTF_8);
 
         assertEquals(3, result.size());
-         /*
-            TODO add extra checks
-         */
+
+        assertThrows(NullPointerException.class, () -> object.getAllItems(null));
+        assertThrows(NullPointerException.class, () -> object.getAllItems(wrongFile));
     }
 }
