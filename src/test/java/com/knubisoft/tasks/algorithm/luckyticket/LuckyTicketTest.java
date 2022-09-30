1981fc8c67def816a1c932b5ad93a0dc865013ba
julivoloshyn <yuliavoloshina02@gmail.com>
2022-09-30 22:10:44 +0300

    lucky ticket tests are updated

--- src/test/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketTest.java
+++ src/test/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketTest.java
@@ -1,22 +1,46 @@
 package com.knubisoft.tasks.algorithm.luckyticket;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * TODO rewrite tests to JUnit 5
- *
- * parametrized tests
- */
 class LuckyTicketTest {
 
     private final LuckyTicket instance = new LuckyTicketImpl();
 
-    /**
-     * TODO optimize tests visibility
-     */
+
+    @ParameterizedTest
+    @ValueSource(strings = {"131asd", "null", "1", "123432", "-555555", "abCd"})
+    void isOdd_ShouldReturnTrueForOddNumbers(String ticket) {
+        assertFalse(instance.checkIsLuckyTicket(ticket));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  "})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String ticket) {
+        assertFalse(instance.checkIsLuckyTicket(ticket));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void isBlank_ShouldReturnTrueForNullInputs(String ticket) {
+        assertFalse(instance.checkIsLuckyTicket(ticket));
+    }
+
+    @Test
+    public void luckyTicketsTest() {
+        assertTrue(instance.checkIsLuckyTicket("123321"));
+        assertTrue(instance.checkIsLuckyTicket("123321"));
+        assertTrue(instance.checkIsLuckyTicket("1234321"));
+        assertTrue(instance.checkIsLuckyTicket("2345432"));
+        assertTrue(instance.checkIsLuckyTicket("23455432"));
+        assertTrue(instance.checkIsLuckyTicket("0000000"));
+    }
+
     @Test
     public void notLuckyTicketsTest() {
         assertFalse(instance.checkIsLuckyTicket(null));
@@ -32,18 +56,4 @@ class LuckyTicketTest {
         assertFalse(instance.checkIsLuckyTicket("-555555"));
         assertFalse(instance.checkIsLuckyTicket("131asd131asd131asd131asd131asd....!@#$%^&*()"));
     }
-
-    /**
-     * TODO optimize tests visibility
-     */
-    @Test
-    public void luckyTicketsTest() {
-        assertTrue(instance.checkIsLuckyTicket("123321"));
-        assertTrue(instance.checkIsLuckyTicket("123321"));
-        assertTrue(instance.checkIsLuckyTicket("1234321"));
-        assertTrue(instance.checkIsLuckyTicket("2345432"));
-        assertTrue(instance.checkIsLuckyTicket("23455432"));
-        assertTrue(instance.checkIsLuckyTicket("0000000"));
-    }
-
 }
